4d9548db252c9f4f0073229ea22f3f128b477a73
Kalle Samuels <charles@derkarl.org>
2019-06-16 16:13:33 -0700

    add examples

---
+++ examples/get.rs
@@ -0,0 +1,11 @@
+
+fn main()
+{
+	let url = std::env::args().nth(1)
+		.expect("url expected as parameter");
+	let mut e = idcurl::get(url::Url::parse(&url).unwrap())
+		.send()
+		.expect("error making request");
+	assert!(e.status().is_success());
+	e.copy_to(&mut std::io::stdout()).unwrap();
+}
